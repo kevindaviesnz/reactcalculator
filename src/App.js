a1938ce4a6e5945fe81f4cf2d9ced1e0af7680de
Kevin <kevind@debian.debian>
2024-03-06 14:20:03 +1300

    ...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 // import './App.css';
+import './Calculator.css';
 import React from 'react';
 
 // npm install mathjs
@@ -9,69 +10,80 @@ import * as math from 'mathjs'
 // npm install react-bootstrap bootstrap
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Alert from 'react-bootstrap/Alert';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
 
 function CalculatorButton(props) {
   return (
-    <>
-      <style type="text/css">
-        {`
-    .btn-flat {
-      background-color: orange;
-      color: white;
-    }
-
-    .btn-xxl {
-      padding: 1rem 1.5rem;
-      font-size: 1.5rem;
-    }
-    `}
-      </style>
-    <Button variant="flat" size="xxl" className="calculatorButton" key={"b"+props.button_number} onClick={props.handleCalculatorButtonClick} value={props.button_number}>
+    <Button id={props.button_id} size="lg" variant="secondary" className="calculatorButton" key={"b"+props.button_number} onClick={props.handleCalculatorButtonClick} value={props.button_number}>
       {props.button_number}
     </Button>
-    </>
   );
 }
 
 
 const CalcPanel = (props) => {
+
   return <div>
-    <div>
-      <CalculatorButton id="clear" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="AC"/>
-      <CalculatorButton handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="+/-"/>
-      <CalculatorButton handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="%"/>
-      <CalculatorButton id="divide" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="/"/>
-    </div>
-    <div>
-      <CalculatorButton id="seven" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="7"/>
-      <CalculatorButton id="eight" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="8"/>
-      <CalculatorButton id="nine" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="9"/>
-      <CalculatorButton id="multiply" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="*"/>
-    </div>
-    <div>
-      <CalculatorButton id="four" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="4"/>
-      <CalculatorButton id="five" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="5"/>
-      <CalculatorButton id="six" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="6"/>
-      <CalculatorButton id="subtract" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="-"/>      
-    </div>
-    <div>
-      <CalculatorButton id="one" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="1"/>
-      <CalculatorButton id="two" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="2"/>
-      <CalculatorButton id="three" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="3"/>
-      <CalculatorButton id="add" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="+"/>            
-    </div>
-    <div>
-      <CalculatorButton id="zero" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="0"/>
-      <CalculatorButton id="decimal" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="."/>
-      <CalculatorButton id="equals" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="="/>
-    </div>
+      <ButtonToolbar>
+        <ButtonGroup className="me-2">
+          <CalculatorButton id="clear" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="AC"/>
+          <CalculatorButton button_number="+/-"/>
+          <CalculatorButton button_number="%"/>
+          <CalculatorButton id="divide" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="/"/>
+        </ButtonGroup>
+      </ButtonToolbar>
+
+      <ButtonToolbar>
+        <ButtonGroup className="me-2">
+          <CalculatorButton id="seven" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="7"/>
+          <CalculatorButton id="eight" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="8"/>
+          <CalculatorButton id="nine" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="9"/>
+          <CalculatorButton id="multiply" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="*"/>
+        </ButtonGroup>
+      </ButtonToolbar>
+
+      <ButtonToolbar>
+        <ButtonGroup className="me-2">
+          <CalculatorButton id="four" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="4"/>
+          <CalculatorButton id="five" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="5"/>
+          <CalculatorButton id="six" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="6"/>
+          <CalculatorButton id="subtract" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="-"/>
+        </ButtonGroup>
+      </ButtonToolbar>
+
+      <ButtonToolbar>
+        <ButtonGroup className="me-2">
+          <CalculatorButton id="one" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="1"/>
+          <CalculatorButton id="two" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="2"/>
+          <CalculatorButton id="three" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="3"/>
+          <CalculatorButton id="add" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="+"/>
+        </ButtonGroup>
+      </ButtonToolbar>
+
+      <ButtonToolbar>
+        <ButtonGroup className="me-2">
+          <CalculatorButton button_id="zero" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="0"/>
+          <CalculatorButton id="decimal" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="."/>
+          <CalculatorButton id="equals" handleCalculatorButtonClick={props.handleCalculatorButtonClick} button_number="="/>
+        </ButtonGroup>
+      </ButtonToolbar>
+
 
   </div>
 }
 
 const CalculatorDisplay = (props) => {
   return <div className="calculatorDisplay" id="display">
-      {props.show}
+      <Alert key="info" variant="info">{props.show}</Alert>
   </div>
 }
 
@@ -112,10 +124,10 @@ class Calculator extends React.Component {
     } else if ("=" === event.target.value) {
       this.setState(prevState => {
         if (prevState.button_presses.length > 0) {
+          const last_number = prevState.button_presses[prevState.button_presses.length-1]
           const sum = math.evaluate(prevState.button_presses.join('')) + ""
-          console.log('Got sum ' + sum)
           return {
-            button_presses: [sum * 1, "+"],
+            button_presses: [sum * 1, "+", last_number],
             display: sum
           }
         }
@@ -141,15 +153,16 @@ class Calculator extends React.Component {
       })
     }
 
-    console.log(this.state.button_presses)
-
   }
 
   render() {
-    return <div id="calculator">
-      <CalculatorDisplay show={this.state.display} />
-      <CalcPanel handleCalculatorButtonClick={this.handleCalculatorButtonClick} />
-    </div>
+    return <Card id="calculator">
+            <ListGroup id="listGroup" style={{backgroundColor: "purple", width: "auto", margin: "0px"}}>
+              <ListGroup.Item style={{width: "auto"}}><CalculatorDisplay show={this.state.display} /></ListGroup.Item>
+              <ListGroup.Item style={{width: "auto"}}><CalcPanel handleCalculatorButtonClick={this.handleCalculatorButtonClick} /></ListGroup.Item>
+            </ListGroup>
+          </Card>
+
   }
 
 
@@ -159,8 +172,10 @@ class Calculator extends React.Component {
 
 function App(props) {
   return (
-    <div className="App">
-        <Calculator />
+    <div>
+    <h1>Calculator Demo</h1>
+    <div style={{backgroundColor: "orange", width: "20%"}}><Calculator /></div>
+    <div><Calculator /></div>
     </div>
   );
 }
